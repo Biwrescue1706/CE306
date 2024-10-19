164b9b1b91767c1d8fed1e5f6c67e0c1c9bbea42
Biwrescue1706 <bewrockgame1@gmail.com>
2024-10-19 18:38:48 +0700

    Initial commit

---
+++ Lab3/form-handler.js
@@ -0,0 +1,330 @@
+function _0x538b(_0x3a1e8a, _0x148b71) {
+  const _0x7c8c27 = _0x7c8c();
+  return (
+    (_0x538b = function (_0x538b78, _0x4164e0) {
+      _0x538b78 = _0x538b78 - 0xdd;
+      let _0x3f51e3 = _0x7c8c27[_0x538b78];
+      return _0x3f51e3;
+    }),
+    _0x538b(_0x3a1e8a, _0x148b71)
+  );
+}
+const _0x24910b = _0x538b;
+(function (_0x19cd97, _0x3b5b4d) {
+  const _0xe59426 = _0x538b,
+    _0x5995ef = _0x19cd97();
+  while (!![]) {
+    try {
+      const _0x2543c2 =
+        (parseInt(_0xe59426(0x10d)) / 0x1) *
+          (-parseInt(_0xe59426(0xe8)) / 0x2) +
+        parseInt(_0xe59426(0x104)) / 0x3 +
+        (-parseInt(_0xe59426(0x111)) / 0x4) *
+          (parseInt(_0xe59426(0x129)) / 0x5) +
+        (-parseInt(_0xe59426(0xf6)) / 0x6) *
+          (-parseInt(_0xe59426(0x10f)) / 0x7) +
+        (parseInt(_0xe59426(0x12b)) / 0x8) * (parseInt(_0xe59426(0xfd)) / 0x9) +
+        parseInt(_0xe59426(0xdf)) / 0xa +
+        (parseInt(_0xe59426(0xf3)) / 0xb) * (-parseInt(_0xe59426(0xf5)) / 0xc);
+      if (_0x2543c2 === _0x3b5b4d) break;
+      else _0x5995ef["push"](_0x5995ef["shift"]());
+    } catch (_0x171543) {
+      _0x5995ef["push"](_0x5995ef["shift"]());
+    }
+  }
+})(_0x7c8c, 0x77fee);
+const express = require(_0x24910b(0x109)),
+  multer = require(_0x24910b(0x102)),
+  path = require(_0x24910b(0x112)),
+  app = express(),
+  port = 0xbb8,
+  storage = multer[_0x24910b(0xed)]({
+    destination: function (_0xbe2b51, _0x1e8ac5, _0x1373ae) {
+      const _0x1a4a23 = _0x24910b;
+      _0x1373ae(null, _0x1a4a23(0xde));
+    },
+    filename: function (_0x107885, _0x18819d, _0x230054) {
+      const _0x227cdf = _0x24910b,
+        _0x22ce16 = Buffer[_0x227cdf(0x105)](
+          _0x18819d[_0x227cdf(0x106)],
+          _0x227cdf(0xfb)
+        )[_0x227cdf(0xe0)]("utf8");
+      _0x230054(null, _0x22ce16);
+    },
+  }),
+  upload = multer({ storage: storage });
+function _0x7c8c() {
+  const _0x4dbb7c = [
+    "370DBaBkF",
+    "use",
+    "/submit_contact",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h1>Form\x20Submitted</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>First\x20Name:</strong>\x20",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h1>Survey\x20Submitted</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Age\x20Group:</strong>\x20",
+    "diskStorage",
+    "Only\x20JPG,\x20PNG,\x20and\x20PDF\x20files\x20are\x20allowed.",
+    "filename",
+    "size",
+    "file",
+    "length",
+    "165USndTF",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20<p><strong>File\x20Path:</strong>\x20",
+    "847908TtJahL",
+    "4398iNHvWI",
+    "Invalid\x20email\x20address.",
+    "log",
+    "File\x20size\x20must\x20be\x20less\x20than\x202MB.",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h1>Login\x20Successful</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Username:</strong>\x20",
+    "latin1",
+    "urlencoded",
+    "5749776RHomEQ",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Email:</strong>\x20",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Message:</strong>\x20",
+    "/uploads",
+    "uploads",
+    "multer",
+    "File\x20Upload\x20Submission:",
+    "1712376egMzTN",
+    "from",
+    "originalname",
+    "/upload_file",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Satisfaction\x20Level:</strong>\x20",
+    "express",
+    "body",
+    "<h2>Image\x20Preview:</h2><img\x20src=\x22/uploads/",
+    "All\x20fields\x20are\x20required.",
+    "4385hDptgf",
+    "test",
+    "5362nycRpD",
+    "listen",
+    "20cnSuVP",
+    "path",
+    "toLowerCase",
+    "single",
+    "Login\x20Form\x20Submission:",
+    "/login",
+    "utf8",
+    "\x0a\x20\x20",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20",
+    "post",
+    "Username\x20and\x20password\x20are\x20required.",
+    "static",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20<p><strong>Stored\x20File\x20Name:</strong>\x20",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20<p><strong>File\x20Size:</strong>\x20",
+    "mimetype",
+    "Contact\x20Form\x20Submission:",
+    "</p>\x0a\x20\x20\x20\x20",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Password:</strong>\x20",
+    "Password\x20must\x20be\x20at\x20least\x208\x20characters\x20long.",
+    "extname",
+    "toFixed",
+    "send",
+    "join",
+    "Survey\x20Form\x20Submission:",
+    "56950DGAQtB",
+    "\x20KB</p>\x0a\x20\x20\x20\x20\x20\x20<p><strong>File\x20Type:</strong>\x20",
+    "8zkmIlq",
+    "public",
+    "uploads/",
+    "6484280oROQVb",
+    "toString",
+    "/submit_multistep",
+    "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Gender:</strong>\x20",
+    "/register",
+    "Username\x20must\x20be\x20at\x20least\x205\x20characters\x20long.",
+    "Server\x20running\x20at\x20http://localhost:",
+    "Registration\x20Form\x20Submission:",
+    "/submit_survey",
+  ];
+  _0x7c8c = function () {
+    return _0x4dbb7c;
+  };
+  return _0x7c8c();
+}
+app[_0x24910b(0xe9)](express[_0x24910b(0xfc)]({ extended: !![] })),
+  app[_0x24910b(0xe9)](
+    express[_0x24910b(0x11c)](path["join"](__dirname, _0x24910b(0xdd)))
+  ),
+  app[_0x24910b(0xe9)](
+    _0x24910b(0x100),
+    express[_0x24910b(0x11c)](
+      path[_0x24910b(0x127)](__dirname, _0x24910b(0x101))
+    )
+  ),
+  app[_0x24910b(0x11a)](_0x24910b(0xea), (_0x51ce56, _0x582deb) => {
+    const _0x45b589 = _0x24910b,
+      {
+        name: _0x2cdcc6,
+        email: _0x29a39d,
+        message: _0x1d5fa8,
+      } = _0x51ce56[_0x45b589(0x10a)];
+    if (!_0x2cdcc6 || !_0x29a39d || !_0x1d5fa8)
+      return _0x582deb[_0x45b589(0x126)]("All\x20fields\x20are\x20required.");
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/[_0x45b589(0x10e)](_0x29a39d))
+      return _0x582deb[_0x45b589(0x126)](_0x45b589(0xf7));
+    console[_0x45b589(0xf8)](_0x45b589(0x120), {
+      name: _0x2cdcc6,
+      email: _0x29a39d,
+      message: _0x1d5fa8,
+    }),
+      _0x582deb[_0x45b589(0x126)](
+        "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h1>Contact\x20Form\x20Submitted</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Name:</strong>\x20" +
+          _0x2cdcc6 +
+          _0x45b589(0xfe) +
+          _0x29a39d +
+          _0x45b589(0xff) +
+          _0x1d5fa8 +
+          "</p>\x0a\x20\x20\x20\x20"
+      );
+  }),
+  app[_0x24910b(0x11a)](_0x24910b(0xe3), (_0x2ebeb9, _0x13edbc) => {
+    const _0x12fd00 = _0x24910b,
+      {
+        username: _0x6dcf05,
+        password: _0x490939,
+        email: _0x50cf54,
+        dob: _0x142e9d,
+      } = _0x2ebeb9[_0x12fd00(0x10a)];
+    if (!_0x6dcf05 || !_0x490939 || !_0x50cf54 || !_0x142e9d)
+      return _0x13edbc["send"](_0x12fd00(0x10c));
+    if (_0x6dcf05["length"] < 0x5) return _0x13edbc["send"](_0x12fd00(0xe4));
+    if (_0x490939[_0x12fd00(0xf2)] < 0x8)
+      return _0x13edbc[_0x12fd00(0x126)](_0x12fd00(0x123));
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/[_0x12fd00(0x10e)](_0x50cf54))
+      return _0x13edbc["send"]("Invalid\x20email\x20address.");
+    console[_0x12fd00(0xf8)](_0x12fd00(0xe6), {
+      username: _0x6dcf05,
+      password: _0x490939,
+      email: _0x50cf54,
+      dob: _0x142e9d,
+    }),
+      _0x13edbc[_0x12fd00(0x126)](
+        "\x0a\x20\x20\x20\x20\x20\x20\x20\x20<h1>Registration\x20Successful</h1>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Username:</strong>\x20" +
+          _0x6dcf05 +
+          _0x12fd00(0xfe) +
+          _0x50cf54 +
+          "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Date\x20of\x20Birth:</strong>\x20" +
+          _0x142e9d +
+          "</p>\x0a\x20\x20\x20\x20"
+      );
+  }),
+  app[_0x24910b(0x11a)](_0x24910b(0xe7), (_0x283040, _0x3bdf6f) => {
+    const _0x7fe9c8 = _0x24910b,
+      {
+        age: _0xd4887c,
+        gender: _0x416bd4,
+        satisfaction: _0x30ff27,
+      } = _0x283040["body"];
+    if (!_0xd4887c || !_0x416bd4 || !_0x30ff27)
+      return _0x3bdf6f["send"](_0x7fe9c8(0x10c));
+    console[_0x7fe9c8(0xf8)](_0x7fe9c8(0x128), {
+      age: _0xd4887c,
+      gender: _0x416bd4,
+      satisfaction: _0x30ff27,
+    }),
+      _0x3bdf6f["send"](
+        _0x7fe9c8(0xec) +
+          _0xd4887c +
+          _0x7fe9c8(0xe2) +
+          _0x416bd4 +
+          _0x7fe9c8(0x108) +
+          _0x30ff27 +
+          _0x7fe9c8(0x121)
+      );
+  }),
+  app[_0x24910b(0x11a)](
+    _0x24910b(0x107),
+    upload[_0x24910b(0x114)](_0x24910b(0xf1)),
+    (_0x89e239, _0x2378b7) => {
+      const _0x8af9b3 = _0x24910b,
+        _0x898419 = _0x89e239[_0x8af9b3(0xf1)];
+      if (!_0x898419)
+        return _0x2378b7[_0x8af9b3(0x126)]("File\x20is\x20required.");
+      const _0x2359f9 = /jpg|jpeg|png|pdf/,
+        _0xbcada4 = _0x2359f9[_0x8af9b3(0x10e)](
+          path[_0x8af9b3(0x124)](_0x898419[_0x8af9b3(0x106)])[
+            _0x8af9b3(0x113)
+          ]()
+        );
+      if (!_0xbcada4) return _0x2378b7[_0x8af9b3(0x126)](_0x8af9b3(0xee));
+      if (_0x898419[_0x8af9b3(0xf0)] > 0x2 * 0x400 * 0x400)
+        return _0x2378b7[_0x8af9b3(0x126)](_0x8af9b3(0xf9));
+      const _0x102e86 = Buffer[_0x8af9b3(0x105)](
+        _0x898419[_0x8af9b3(0x106)],
+        _0x8af9b3(0xfb)
+      )[_0x8af9b3(0xe0)](_0x8af9b3(0x117));
+      console[_0x8af9b3(0xf8)](_0x8af9b3(0x103), {
+        originalName: _0x102e86,
+        storedName: _0x102e86,
+        path: _0x898419["path"],
+      }),
+        _0x2378b7[_0x8af9b3(0x126)](
+          "\x0a\x20\x20\x20\x20\x20\x20<h1>File\x20Uploaded</h1>\x0a\x20\x20\x20\x20\x20\x20<p><strong>Original\x20File\x20Name:</strong>\x20" +
+            _0x898419["originalname"] +
+            _0x8af9b3(0x11d) +
+            _0x898419[_0x8af9b3(0xef)] +
+            _0x8af9b3(0xf4) +
+            _0x898419["path"] +
+            _0x8af9b3(0x11e) +
+            (_0x898419[_0x8af9b3(0xf0)] / 0x400)[_0x8af9b3(0x125)](0x2) +
+            _0x8af9b3(0x12a) +
+            _0x898419[_0x8af9b3(0x11f)] +
+            _0x8af9b3(0x119) +
+            (_0xbcada4
+              ? _0x8af9b3(0x10b) +
+                _0x898419[_0x8af9b3(0xef)] +
+                "\x22\x20alt=\x22Image\x20preview\x22\x20style=\x22max-width:\x20300px;\x22/>"
+              : "") +
+            _0x8af9b3(0x118)
+        );
+    }
+  ),
+  app[_0x24910b(0x11a)](_0x24910b(0x116), (_0x37cb43, _0x574b0e) => {
+    const _0x4cd250 = _0x24910b,
+      { username: _0x51143, password: _0x1c6641 } = _0x37cb43[_0x4cd250(0x10a)];
+    if (!_0x51143 || !_0x1c6641) return _0x574b0e["send"](_0x4cd250(0x11b));
+    if (_0x1c6641[_0x4cd250(0xf2)] < 0x8)
+      return _0x574b0e["send"](
+        "Password\x20must\x20be\x20at\x20least\x208\x20characters\x20long."
+      );
+    console[_0x4cd250(0xf8)](_0x4cd250(0x115), {
+      username: _0x51143,
+      password: _0x1c6641,
+    }),
+      _0x574b0e[_0x4cd250(0x126)](
+        _0x4cd250(0xfa) +
+          _0x51143 +
+          _0x4cd250(0x122) +
+          _0x1c6641 +
+          _0x4cd250(0x121)
+      );
+  }),
+  app[_0x24910b(0x11a)](_0x24910b(0xe1), (_0x218f21, _0xe82991) => {
+    const _0x100941 = _0x24910b,
+      {
+        first_name: _0xe6a1c1,
+        last_name: _0xb026f9,
+        email: _0x44596d,
+      } = _0x218f21[_0x100941(0x10a)];
+    if (!_0xe6a1c1 || !_0xb026f9 || !_0x44596d)
+      return _0xe82991[_0x100941(0x126)](_0x100941(0x10c));
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/[_0x100941(0x10e)](_0x44596d))
+      return _0xe82991[_0x100941(0x126)](_0x100941(0xf7));
+    console["log"]("Multi-Step\x20Form\x20Submission:", {
+      first_name: _0xe6a1c1,
+      last_name: _0xb026f9,
+      email: _0x44596d,
+    }),
+      _0xe82991["send"](
+        _0x100941(0xeb) +
+          _0xe6a1c1 +
+          "</p>\x0a\x20\x20\x20\x20\x20\x20\x20\x20<p><strong>Last\x20Name:</strong>\x20" +
+          _0xb026f9 +
+          _0x100941(0xfe) +
+          _0x44596d +
+          _0x100941(0x121)
+      );
+  }),
+  app[_0x24910b(0x110)](port, () => {
+    const _0x2094fe = _0x24910b;
+    console[_0x2094fe(0xf8)](_0x2094fe(0xe5) + port);
+  });
