eed2647140cc28dcd7a0d58362c2a400e387faa6
Biwrescue1706 <bewrockgame1@gmail.com>
2024-10-28 11:32:17 +0700

    Initial commit

---
+++ Lab10 - Object/Lab10.js
@@ -0,0 +1,33 @@
+const here = {
+    name : "My Hero",
+    level: 20,
+    coins: 2000,
+    weapons:{
+        machinegun : {
+            ammo : 1000,
+            damage : 20
+        },
+        sword : {
+            damage : 100
+        },
+        shield : {
+            protect : 3000
+        },
+        BFG : {
+            ammo : 5000,
+            damage : 40
+        },
+        rocketLauncher : {
+            rocket1 : {
+                damage : 100,
+                speed : 40,
+                ammo : 20
+            },
+            rocket2 : {
+                damage : 200,
+                speed : 5,
+                ammo : 300
+            }
+        }
+    }
+};
