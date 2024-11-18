d62b4c61e8903540a4a19a22001fa2bd1305e776
Phuwanat_Biw <bewrockgame1@gmail.com>
2024-11-18 11:27:59 +0700

    sum

--- Lab14 - DOM/Lab14.js
+++ Lab14 - DOM/Lab14.js
@@ -19,9 +19,28 @@
 //     document.body.appendChild(h1);
 //     document.body.appendChild(ul);
 // });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const p = document.createElement('p');
+//     const text = document.createTextNode('Hello, my name is "Phuwanat "');
+//     p.appendChild(text);
+//     const ul = document.createElement('ul');
+
+//     for (let i = 1; i <= 8; i++) {
+//         const li = document.createElement('li');
+//         const txt = document.createTextNode(i % 2 === 0 ? `Even` : `Odd`);
+//         li.appendChild(txt);
+//         ul.appendChild(li);
+//     }
+//     document.body.appendChild(p);
+//     document.body.appendChild(ul);
+
+// });
+
 function buttonClick() {
     //ค้นหา element
-    //1)ค้นหา id
+    //1)ค้นหาผ่าน id
     const p1 = document.getElementById("p1");
     p1.innerText = "Another Test";
     p1.style.color = "red";
@@ -30,9 +49,14 @@ function buttonClick() {
 
     //2 ค้นหาผ่านชนิดของ tag 
     const liCollection = document.getElementsByTagName('li');
-    for(let li of liCollection){
-        if(li.innerText == "Even"){
+    for (let li of liCollection) {
+        if (li.innerText == "Even") {
             li.style.color = "blue";
         }
+    }
+    //ค้นหาผ่าน Class
+    const elements = document.getElementsByClassName('yellow')
+    for (let el of elements){
+        el.style.backgroundColor = "yellow"
     }
 }
